617812aa22440c210aca69c5208c69658222a949
Gabriel Godoy <gabyreload@gmail.com>
2023-07-24 22:02:58 -0300

    Update endpoints and v1.1.3

--- src/app.js
+++ src/app.js
@@ -6,9 +6,7 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.use("/new-feature", (req, res) => res.send("<h1>New Feature V1.1.1</h1>"));
-app.use("/api/testing", testingRouter);
-app.use("/", (req, res) => res.send("<h1>HELLO CI-CD</h1>"));
+app.use("/", (req, res) => res.send("<h1>HELLO CI-CD V1.1.3</h1>"));
 
 const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => console.log(`Listening on port ${PORT}`));
